500e0b0a8107da8e592b352ab6c383aaa9d73e28
Nicole CHOW-YUEN-MIN <nchow-yu@e1r5p20.clusters.42paris.fr>
2022-09-03 16:02:16 +0200

    minishell => lexer en cours

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,7 +6,7 @@
 /*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:40:06 by nchow-yu          #+#    #+#             */
-/*   Updated: 2022/09/03 15:51:43 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2022/09/03 16:02:12 by nchow-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	ft_get_word(t_data *data, int i)
 	char	*str;
 
 	str = data->readline;
-	printf("str = %s\n", str);//a enlever
+	//printf("str = %s\n", str);//a enlever
 	if (str[i] == 39 || str[i] == 34)
 	{
 		if (ft_word_quote(str, i, str[i]) == 0)
@@ -55,11 +55,11 @@ int	ft_get_word(t_data *data, int i)
 	if (str[i] >= 33 && str[i] <= 126 && ft_check_metachar(str, i))
 	{
 		word = ft_substr(str, i, ft_wdlen(str, i));
-		printf("wdlen = %d\n", ft_wdlen(str, i));
-		printf("word substr: %s\n", word);//a enlever
+		//printf("wdlen = %d\n", ft_wdlen(str, i));
+		//printf("word substr: %s\n", word);//a enlever
 		if (word == NULL)
 			ft_exit(data); /*free(word);*/
-		printf("word dans get_word: %s\n", word);
+		//printf("word dans get_word: %s\n", word);
 		ft_add_token(word, data, LITERAL);
 		return (ft_wdlen(str, i));
 	}
--- srcs/token.c
+++ srcs/token.c
@@ -6,7 +6,7 @@
 /*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 20:20:13 by nchow-yu          #+#    #+#             */
-/*   Updated: 2022/09/03 15:49:07 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2022/09/03 16:02:03 by nchow-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_get_token(t_data *data, char *rdline, int i, int strlen)
 	if (wd == NULL)
 		ft_exit(data);
 	//free(wd);
-	printf("word dans get_token: %s\n", wd);//a enlever
+	//printf("word dans get_token: %s\n", wd);//a enlever
 	ft_add_token(wd, data, LITERAL);
 	return (0);
 }
@@ -30,7 +30,7 @@ void	ft_add_token(char *word, t_data *data, int type)
 {
 	t_token	*new;
 
-	printf("word: %s\n", word);//a enlever
+	//printf("word: %s\n", word);//a enlever
 	new = malloc(sizeof(t_token));
 	if (new == NULL)
 		ft_exit(data);
@@ -43,12 +43,16 @@ void	ft_add_token(char *word, t_data *data, int type)
 void	show_token(t_data *data)
 {
 	t_token	*tmp;
+	int		i;
 
 	tmp = data->token;
+	i = 0;
 	while (tmp != NULL)
 	{
 		printf("token: %s\n", tmp->value);//a enlever
 		printf("type: %d\n", tmp->type);//a enlever
+		printf("i = %d\n", i);//a enlever
 		tmp = tmp->next;
+		i++;
 	}
 }
