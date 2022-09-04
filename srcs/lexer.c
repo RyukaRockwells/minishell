eff4c0d12f3f2cd0e7201567dfeb6234d79e2a23
Nicole CHOW-YUEN-MIN <nchow-yu@e1r1p6.clusters.42paris.fr>
2022-09-04 23:27:00 +0200

    minishell => parser en cours + qq leaks

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,7 +6,7 @@
 /*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:40:06 by nchow-yu          #+#    #+#             */
-/*   Updated: 2022/09/04 21:13:50 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2022/09/04 22:49:57 by nchow-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	ft_get_word(t_data *data, int i)
 			ft_exit(data); /*free(word);*/
 		//printf("word dans get_word: %s\n", word);
 		ft_add_token(word, data, LITERAL);
-		free(word);
+		//free(word);
 		return (ft_wdlen(str, i));
 	}
 	//free(word);
@@ -83,7 +83,7 @@ int	ft_get_sep(t_data *data, int i)
 		if (sep == NULL)
 			ft_exit(data);
 		ft_add_token(sep, data, PIPE);
-		free(sep);
+		//free(sep);
 	}
 	return (0);
 }
--- srcs/parser.c
+++ srcs/parser.c
@@ -6,11 +6,21 @@
 /*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 12:01:35 by nchow-yu          #+#    #+#             */
-/*   Updated: 2022/09/04 12:10:54 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2022/09/04 23:26:05 by nchow-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int	ft_pipe(t_token *tok)
+{
+	if (tok->next == NULL)
+		return (PIPE_ERROR);
+	if (tok->next->type == PIPE)
+		return (PIPE_ERROR);
+	if (tok->next == NULL && tok->type != LITERAL)
+		return (PIPE_ERROR);
+}
 
 int	ft_parser(t_data *data)
 {
@@ -21,4 +31,12 @@ int	ft_parser(t_data *data)
 	error_code = ft_pre_check(tmp);
 	if (error_code > 0)
 		return (error_code);
+	while (tmp != NULL)
+	{
+		if (tmp->type == PIPE)
+			error_code = ft_pipe(tmp);
+		if (error_code > 0)
+			return (error_code);
+		tmp = tmp->next;
+	}
 }
